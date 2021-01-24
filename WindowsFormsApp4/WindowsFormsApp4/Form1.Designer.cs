b03e17055c869f373e40fbe7c0f147c3e67cc8bc
bilenkobogdan1 <56382520+bilenkobogdan1@users.noreply.github.com>
2021-01-24 18:12:40 +0300

    Add files via upload

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(28, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "відкрити";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 359);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(161, 394);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "конвертувати SAX";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(661, 394);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 46);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "зберегти ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(400, 394);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(255, 46);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "конвертувати LINQ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 479);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp4
+{ 
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private string Perform(string text)
+        {
+            text = Regex.Replace(text, "^(.*?)$", "<h2><p>$1</p><h2>\r\n", RegexOptions.Multiline);
+            return text;
+        }
+        private string wayUpgreate(string text)
+        {
+            text = Regex.Replace(text, "xml", "html", RegexOptions.Multiline);
+            return text;
+        }
+        class User
+        {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public DateTime Registered { get; private set; }
+        public string Faculty { get; private set; }
+        public string Residence { get; private set; }
+        public int Course { get; private set; }
+        
+            public User(string name, int age, DateTime registered,string faculty,string residence,int course)
+        {
+            Name = name;
+            Age = age;
+            Registered = registered;
+            Faculty = faculty;
+            Residence = residence;
+            Course = course;
+
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, вік {1}, дата заселення {2}, місце заселення{3} \n факультет {4}, курс {5}", Name, Age, Registered.ToShortDateString(), Residence, Faculty,Course);
+        }
+
+    }
+    string textXml = "";
+     string way;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OPF = new OpenFileDialog();
+            //OPF.Multiselect = true;
+           
+            OPF.Filter = "Файлы html|*.html|Файлы xml|*.xml";
+            if (OPF.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in OPF.FileNames)
+                {
+                    MessageBox.Show(file);
+                   
+                    way = file;
+                    StreamReader streamReader = new StreamReader(file);
+                    
+                   while (!streamReader.EndOfStream)
+                   {
+                       textXml = textXml+streamReader.ReadLine()+ "\r\n";
+                      
+                        
+                    }
+                   
+                    textBox1.Text =way;
+
+                }
+            }
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                 List<User> users = new List<User>();
+
+            using (XmlReader xr = XmlReader.Create(way))
+            {
+                string faculty="";
+                string name = "";
+                string residence="";
+                int age = 0;
+                int course = 0;
+                DateTime registered = DateTime.Now;
+                string element = "";
+                while (xr.Read())
+                {
+                    // reads the element
+                    if (xr.NodeType == XmlNodeType.Element)
+                    {
+                        element = xr.Name; // the name of the current element
+                        if (element == "user")
+                        {
+                            age = int.Parse(xr.GetAttribute("age"));
+                        }
+                    }
+                    // reads the element value
+                    else if (xr.NodeType == XmlNodeType.Text)
+                    {
+                        switch (element)
+                        {
+                            case "name":
+                                name = xr.Value;
+                                break;
+                            case "registered":
+                                registered = DateTime.Parse(xr.Value);
+                                break;
+                            case "faculty":
+                                faculty = xr.Value;
+                                break;
+                            case "residence":
+                                residence = xr.Value;
+                                break;
+                            case "course":
+                                course = int.Parse(xr.Value);
+                                break;
+                        }
+                    }
+                    // reads the closing element
+                    else if ((xr.NodeType == XmlNodeType.EndElement) && (xr.Name == "user"))
+                        users.Add(new User(name, age, registered,faculty, residence,course));
+                    string filenew = "";
+                    foreach (User u in users)
+                    {
+                        filenew += u+ "\r\n";                  
+                    }
+                    textBox1.Text = filenew;
+                }
+               
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Perform(textBox1.Text);
+            File.WriteAllText(wayUpgreate(way), textBox1.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.Text += "LINQ\r\n";
+            XDocument xdoc = XDocument.Load(way);
+            foreach (XElement phoneElement in xdoc.Element("users").Elements("user"))
+            {
+                XAttribute ageAttribute = phoneElement.Attribute("age");
+                XElement nameElement = phoneElement.Element("name");
+                XElement registeredElement = phoneElement.Element("registered");
+                XElement facultyElement = phoneElement.Element("faculty");
+                XElement courseElement = phoneElement.Element("course");
+                XElement residenceElement = phoneElement.Element("residence");
+
+
+                if (ageAttribute != null && nameElement != null && registeredElement != null && facultyElement!= null && courseElement != null &&  residenceElement != null)
+                {
+                   
+                    textBox1.Text += ($"Компания: {nameElement.Value}");
+                    textBox1.Text += ($"дата заселення {ageAttribute.Value}");
+                    textBox1.Text += ($"місце заселення \r\n {registeredElement.Value}");
+                    textBox1.Text += ($"дата заселення {residenceElement.Value}");
+                    textBox1.Text += ($"факультет {facultyElement.Value}");
+                    textBox1.Text += ($"курс {courseElement.Value}");
+                    
+                }
+                
+            }
+        }
+    }
+}
